03c7a171973c7def21c1812635463a51c955bdc5
Frank Lavery <frank.lavery@westnet.com.au>
2025-04-28 20:22:38 +0800

    correct lib/generated build errors

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    // Will still allow production build with type errors!
-    ignoreBuildErrors: true,
-  },
+  typescript: {},
 };
 
 export default nextConfig;
